96901f8fe20b056992bbb4cd6c8680411a47817c
zeenetzexie <nazarizorodzai@gmail.com>
2026-06-09 23:20:13 +0200

    temp: get ipn id helper

---
+++ app/api/pesapal/get-ipn-id/route.js
@@ -0,0 +1,22 @@
+export async function GET() {
+  const consumerKey = process.env.PESAPAL_CONSUMER_KEY;
+  const consumerSecret = process.env.PESAPAL_CONSUMER_SECRET;
+  const apiBase = 'https://cybqa.pesapal.com/pesapalv3/api';
+
+  // Get token
+  const tokenRes = await fetch(`${apiBase}/Auth/RequestToken`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+    body: JSON.stringify({ consumer_key: consumerKey, consumer_secret: consumerSecret })
+  });
+
+  const { token } = await tokenRes.json();
+
+  // Get IPN list
+  const ipnRes = await fetch(`${apiBase}/URLSetup/GetIpnList`, {
+    headers: { 'Authorization': `Bearer ${token}`, 'Accept': 'application/json' }
+  });
+
+  const ipnData = await ipnRes.json();
+  return Response.json(ipnData);
+}
